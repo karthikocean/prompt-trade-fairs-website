94a8c3b8be670e9effed10a5fe40b3f5555fa368
Deepan-Senthil <deepan@oceansoftteam.in>
2026-08-25 12:32:34 +0530

    axios apiClient configuration and set default environment to local

--- src/config/apiClient.js
+++ src/config/apiClient.js
@@ -8,9 +8,9 @@ let server = "";
 
 switch (APP_ENV) {
     case "dev":
-        IMAGE_BASE_URL = "http://192.168.1.17:2001/public";
-        BASE_URL = "http://192.168.1.17:2001/api/website";
-        server = "http://192.168.1.17:2001";
+        IMAGE_BASE_URL = "http://192.168.88.26:2001/public";
+        BASE_URL = "http://192.168.88.26:2001/api/website";
+        server = "http://192.168.88.26:2001";
         break;
 
     case "production":
@@ -21,9 +21,9 @@ switch (APP_ENV) {
 
     case "local":
     default:
-        IMAGE_BASE_URL = "http://localhost:2001/public";
-        BASE_URL = "http://localhost:2001/api/website";
-        server = "http://localhost:2001";
+        IMAGE_BASE_URL = "http://192.168.88.26:2001/public";
+        BASE_URL = "http://192.168.88.26:2001/api/website";
+        server = "http://192.168.88.26:2001";
         break;
 }
 
